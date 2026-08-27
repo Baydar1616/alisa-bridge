383cc7751540a7c71c5c02497a7671db5a4091ae
Baydar <bakhtbaydar8@gmail.com>
2026-08-27 23:16:04 +0500

    Add AliceBridgeApplication.kt

---
+++ app/src/main/java/com/alisabridge/AliceBridgeApplication.kt
@@ -0,0 +1,10 @@
+package com.alisabridge
+
+import android.app.Application
+
+class AliceBridgeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize app-wide components if needed
+    }
+}
