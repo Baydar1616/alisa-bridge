3f2624ad851cb308f6f16315cd968f1f8598a854
Baydar <bakhtbaydar8@gmail.com>
2026-08-27 23:11:03 +0500

    Add root build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Root build.gradle.kts
+plugins {
+    kotlin("jvm") version "1.9.10" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+allprojects {
+}
